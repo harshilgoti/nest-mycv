e5d88707daa3b63378f07f79814e87f2a20d4fec
Harshil Goti <hgoti@plusonex.com>
2024-04-02 11:15:03 +0530

    first commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+  AfterInsert,
+  AfterUpdate,
+  AfterRemove,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Insert user with id:', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Update user with id:', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Remove user with id:', this.id);
+  }
+}
